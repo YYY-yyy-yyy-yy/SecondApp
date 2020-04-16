19faf49ff867d857a9223d268b2b3e7ae1823a34
YYY-yyy-yyy-yy <1980310469@qq.com>
2020-04-16 18:49:11 +0800

    HuiLv

--- app/src/main/java/com/swufe/secondapp/RateActivity.java
+++ app/src/main/java/com/swufe/secondapp/RateActivity.java
@@ -1,10 +1,8 @@
 package com.swufe.secondapp;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,24 +18,5 @@ public class RateActivity extends AppCompatActivity {
         rmb = (EditText) findViewById(R.id.rmb);
         show = (TextView) findViewById(R.id.showOut);
 
-    }
-    public void onClick(View btn){
-        String str = rmb.getText().toString();
-        float r = 0;
-        if(str.length()>0){
-            r = Float.parseFloat(str);
-        }else{
-            Toast.makeText(this,"请输入金额 ",Toast.LENGTH_SHORT).show();
-        }
-        float val=0;
-        if(btn.getId()==R.id.btn_dollar){
-            val = r*(1/6.7f);
-        }else if(btn.getId()==R.id.btn_euro){
-           val = r*(1/11f);
-        }else if(btn.getId()==R.id.btn_won){
-             val = r*500;
-        }
-        float res = (float) (Math.round(val * 100)) / 100;
-        show.setText(String.valueOf(res));
     }
 }
