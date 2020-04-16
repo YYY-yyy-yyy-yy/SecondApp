a388ed5fc3f3b2fdce91162b72379bb407231ed6
YYY-yyy-yyy-yy <1980310469@qq.com>
2020-04-16 14:59:59 +0800

    JiFen+

--- app/src/main/java/com/swufe/secondapp/JifenActivity.java
+++ app/src/main/java/com/swufe/secondapp/JifenActivity.java
@@ -10,30 +10,50 @@ import android.widget.TextView;
 public class JifenActivity extends AppCompatActivity {
     //重新上传
     TextView score;
+    TextView score2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jifen);
         score = (TextView) findViewById(R.id.score);
-
+        score2 = (TextView) findViewById(R.id.score2);
     }
-    public void btnAdd1(View click){
-        showScore(1);
+    public void btnAdd1(View btn){
+        if(btn.getId()==R.id.btn_1) {
+            showScore(1);
+        }else{
+            showScore2(1);
+        }
     }
-    public void btnAdd2(View click){
-        showScore(2);
+    public void btnAdd2(View btn){
+        if(btn.getId()==R.id.btn_2) {
+            showScore(2);
+        }else{
+            showScore2(2);
+        }
     }
-    public void btnAdd3(View click){
-        showScore(3);
+    public void btnAdd3(View btn){
+        if(btn.getId()==R.id.btn_3) {
+            showScore(3);
+        }else{
+            showScore2(3);
+        }
     }
-    public void btnReset(View click){
+    public void btnReset(View btn){
         score.setText("0");
+        score2.setText("0");
     }
     private void showScore(int inc){
         Log.i("show","inc="+inc);
         String oldScore = (String) score.getText();
         int newScore=Integer.parseInt(oldScore) + inc;
         score.setText(""+newScore);
+    }
+    private void showScore2(int inc){
+        Log.i("show2","inc="+inc);
+        String oldScore = (String) score.getText();
+        int newScore=Integer.parseInt(oldScore) + inc;
+        score2.setText(""+newScore);
 
     }
 }
